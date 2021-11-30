2114a9b10b747d044d0567ecabce1ea6b9ac83af
Celtuzed <uter222222@gmail.com>
2021-11-30 19:13:26 +0300

    made a selection of books that can be downloaded

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 import requests
 from bs4 import BeautifulSoup
@@ -8,12 +9,27 @@ from main_parser_logic import parse_book_page
 
 if __name__ == '__main__':
 
+    parser = argparse.ArgumentParser(description='Прога парсит инфу о книге по её id и скачивает её же')
+    parser.add_argument(
+            '-si', '--start_id',
+            default=1,
+            help='С какой книги начинать парсить',
+            type=int
+                        )
+    parser.add_argument(
+            '-ei', '--end_id',
+            default=10,
+            help='До какой книги будет парсить(включительно)',
+            type=int
+                        )
+    args = parser.parse_args()
+
     books_folder = "books"
     os.makedirs(books_folder, exist_ok=True)
     images_folder = "images"
     os.makedirs(images_folder, exist_ok=True)
 
-    for book_id in range(1, 11):
+    for book_id in range(args.start_id, args.end_id):
 
         url = f"https://tululu.org/txt.php?id={book_id}"
 
@@ -22,6 +38,9 @@ if __name__ == '__main__':
             response.raise_for_status()
             check_for_redirect(response)
             book_information, image_link = parse_book_page(book_id)
+
+            filename = book_information['book_name']
+            author_name = book_information['author_name']
             print(book_information)
             #download_txt(url, filename, books_folder, book_id)
             #download_image(image_link, filename, images_folder, book_id)
