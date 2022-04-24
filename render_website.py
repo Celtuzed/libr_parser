162f147bd2008b15acf115974b9c65637cf8a1fa
Celtuzed <76615478+Celtuzed@users.noreply.github.com>
2022-04-25 01:45:05 +0300

    Render more pages with new genres

--- render_website.py
+++ render_website.py
@@ -7,33 +7,42 @@ from more_itertools import chunked
 
 
 COLUMNS_ON_PAGE = 2
-LINES_ON_PAGE = 10
+LINES_ON_PAGE = 8
 
 
 def on_reload():
 
-    env = Environment(
-        loader=FileSystemLoader("."),
-        autoescape=select_autoescape(["html", "xml"])
-    )
-    template = env.get_template('template.html')
+    genres = [
+            "l55",  # Научная фантастика
+            "l77",  # Прочие детективы
+            "l101",  # Триллеры
+            "l8"  # Боевики
+            ]
 
-    with open("results/books_information.json", "r", encoding='utf8') as information:
-        books = json.load(information)
-
-    books_for_line = list(chunked(books, COLUMNS_ON_PAGE))
-    pages = list(chunked(books_for_line, LINES_ON_PAGE))
-    pages_number = len(pages)
-
-    for page_number, books_for_page in enumerate(pages, 1):
-
-        rendered_page = template.render(
-            books=books_for_page,
-            this_page=page_number,
-            pages_number=pages_number
+    for genre in genres:
+        env = Environment(
+            loader=FileSystemLoader("."),
+            autoescape=select_autoescape(["html", "xml"])
         )
-        with open(f"pages/index{page_number}.html", "w", encoding='utf8') as file:
-            file.write(rendered_page)
+        template = env.get_template('template.html')
+
+        with open(f"results/{genre}_books_information.json", "r", encoding='utf8') as information:
+            books = json.load(information)
+
+        books_for_line = list(chunked(books, COLUMNS_ON_PAGE))
+        pages = list(chunked(books_for_line, LINES_ON_PAGE))
+        pages_number = len(pages)
+
+        for page_number, books_for_page in enumerate(pages, 1):
+
+            rendered_page = template.render(
+                page_genre=genre,
+                books=books_for_page,
+                this_page=page_number,
+                pages_number=pages_number
+            )
+            with open(f"pages/{genre}_index{page_number}.html", "w", encoding='utf8') as file:
+                file.write(rendered_page)
 
 
 if __name__ == '__main__':
